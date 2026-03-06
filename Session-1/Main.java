7a21c79fe615ebcc1e6b41ef28789b87bc117ef1
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-03-06 13:43:26 +0530

    Intial update

---
+++ Session-1/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner Scn = new Scanner(System.in);
+        int n = Scn.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = Scn.nextInt();
+        }
+        int largest;
+        int SecLargest;
+        
+    }
+}
